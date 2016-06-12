d38ec52a3bc47056b722c3aa2cfab8ee13cd70fe
Swarnim <swarn7497@gmail.com>
2016-06-12 19:33:29 +0530

    initial commit for new design and setup

--- app/src/main/java/com/example/swarnim/signup/MainActivity.java
+++ app/src/main/java/com/example/swarnim/signup/MainActivity.java
@@ -2,12 +2,39 @@ package com.example.swarnim.signup;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText fullNameText;
+    EditText emailIDText;
+    EditText passwordText;
+    Spinner projectSpinner;
+    Button createProfile;
+    TextView alreadyAccount;
+    ArrayAdapter<String> arrayAdapterSpinner;
+
+    String projectSpinnerValue[] = {"Doctor"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fullNameText = (EditText) findViewById(R.id.fullNameText);
+        emailIDText = (EditText) findViewById(R.id.emailIDText);
+        passwordText = (EditText) findViewById(R.id.passwordText);
+        projectSpinner = (Spinner) findViewById(R.id.doctorSpinner);
+        createProfile = (Button) findViewById(R.id.createProfile);
+        alreadyAccount = (TextView) findViewById(R.id.alreadyAccount);
+
+        arrayAdapterSpinner = new ArrayAdapter<String>(this,R.layout.spinner_text_styles,projectSpinnerValue);
+        projectSpinner.setAdapter(arrayAdapterSpinner);
+
     }
 }
